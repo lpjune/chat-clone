3ed010727bc4a2c248c90adfac2be12b6f0ba852
lpjune <laurenporsini@gmail.com>
2020-10-23 12:56:01 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require("apollo-server");
+const { sequelize } = require("./models");
+
+// The GraphQL schema
+const typeDefs = require("./graphql/typeDefs");
+
+// A map of functions which return data for the schema.
+const resolvers = require("./graphql/resolvers");
+
+const contextMiddleware = require("./util/contextMiddleware")
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: contextMiddleware,
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+
+    sequelize
+        .authenticate()
+        .then(() => console.log("Database connected!"))
+        .catch((err) => console.log(err));
+});
